adc63be7ba03c06959d2b197c036ad8a186907cc
Tanaka878 <musungaretanaka@gmail.com>
2024-10-29 18:27:18 +0200

    change password

--- src/main/java/SpringSecurity/WebConfig.java
+++ src/main/java/SpringSecurity/WebConfig.java
@@ -1,9 +1,16 @@
 package SpringSecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    // No CORS configuration here, which means CORS is effectively disabled.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Allow localhost:3000 during development
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);  // Set to true if cookies are need
+    }
 }
