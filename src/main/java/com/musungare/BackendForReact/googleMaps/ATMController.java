6b6999d289ccf9d85cb34494fad17078dbdb01d2
Tanaka878 <musungaretanaka@gmail.com>
2024-11-09 18:58:04 +0200

    ATM functionaliity

--- src/main/java/com/musungare/BackendForReact/googleMaps/ATMController.java
+++ src/main/java/com/musungare/BackendForReact/googleMaps/ATMController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/atm")
-@CrossOrigin("*")  // Allow all origins (use specific origins in production)
+@CrossOrigin("*")
 public class ATMController {
 
     private final GoogleMapsService googleMapsService;
@@ -15,13 +15,11 @@ public class ATMController {
         this.googleMapsService = googleMapsService;
     }
 
-    // Endpoint to find nearby ATMs based on latitude and longitude
     @GetMapping("/find")
     public String findATMs(@RequestParam double latitude, @RequestParam double longitude) {
         return googleMapsService.getATMLocations(latitude, longitude);
     }
 
-    // Endpoint to geocode an address (get latitude and longitude)
     @GetMapping("/geocode")
     public String geocode(@RequestParam String address) {
         return googleMapsService.getGeocode(address);
