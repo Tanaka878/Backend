dc6fafe799b5ebacec01127da44a3e1485f4a1b3
Tanaka878 <musungaretanaka@gmail.com>
2024-11-22 13:09:29 +0200

    adding loan components

--- src/main/java/com/musungare/BackendForReact/BankAccout/Loan/Controller/LoanController.java
+++ src/main/java/com/musungare/BackendForReact/BankAccout/Loan/Controller/LoanController.java
@@ -4,13 +4,11 @@ import com.musungare.BackendForReact.BankAccout.Loan.Loan;
 import com.musungare.BackendForReact.BankAccout.Loan.Service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest/loan/")
+@CrossOrigin("*")
 public class LoanController {
 
     private final LoanService loanService;
